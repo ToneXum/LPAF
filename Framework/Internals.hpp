f4780f875856bc8ca46faf87622053472ff0d99a
ToneXum <toni-stein11@gmx.de>
2023-12-08 23:47:02 +0100

    update all files to include the license

--- Framework/Internals.hpp
+++ Framework/Internals.hpp
@@ -1,9 +1,31 @@
-#pragma once
+// MIT License
+//
+// Copyright(c) 2023 ToneXum
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright noticeand this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 /// About this file:
 /// 
 /// This header is containing the internal symbols that are not supposed to be
 /// exposed to the end user.
+
+#pragma once
 
 // throw away a bunch of windows garbage
 #ifndef FULL_WINTARD
